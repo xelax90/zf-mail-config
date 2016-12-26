4e6c9a51c77bdf12f3ba8555274b6a29b13a0e62
Aleksandr <schurix@gmx.de>
2016-12-26 02:14:17 +0100

    Adjust example config

--- config/xelax-mail-config.global.php
+++ config/xelax-mail-config.global.php
@@ -4,9 +4,11 @@ use Zend\Mail\Transport\Smtp;
 
 return [
 	Module::CONFIG_KEY => [
-		'type'				=> Smtp::class,
-		'name'              => 'localhost',
-		'host'              => '127.0.0.1',
-		'connection_class'  => 'login'
+		'type'    => Smtp::class,
+		'options' => [
+			'name'              => 'localhost',
+			'host'              => '127.0.0.1',
+			'connection_class'  => 'login'
+		]
 	],
 ];
